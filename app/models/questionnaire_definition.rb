13345337da6c22ad64563c5f1e1c868f1ec367e5
Satana de Sant'Ana <satana@skylittlesystem.org>
2025-10-26 22:38:22 +0000

    implement QuestionnaireDefinition

--- app/models/questionnaire_definition.rb
+++ app/models/questionnaire_definition.rb
@@ -1,2 +1,21 @@
 class QuestionnaireDefinition < ApplicationRecord
+  after_create :create_data_table
+
+  def data_table_name
+    "questionnaire_#{id}"
+  end
+
+  def create_data_table
+    self.class.connection.create_table(data_table_name) do |table|
+      schema.each do |question, specs|
+        table.send(specs["type"], question)
+      end
+    end
+  end
+
+  def data_model
+    Class.new(ApplicationRecord).tap do |klass|
+      klass.table_name = data_table_name
+    end
+  end
 end
